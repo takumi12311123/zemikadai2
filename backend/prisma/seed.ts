49648f612cf2929be0cecd7804435f23e5e08f7e
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-07-17 18:38:41 +0900

    backend schemaの変更とseedの修正

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -5,32 +5,36 @@ async function main() {
   const takuaka = await prisma.user.create({
     data: {
       id: 'user-id-1',
-      name: 'takuaka',
+      name: 'サカス',
       email: 'example.com',
+      description: '私の名前はサカスです',
     },
   });
 
   const takuami = await prisma.user.create({
     data: {
-      id: 'user-id-3',
-      name: 'takuaka',
+      id: 'user-id-2',
+      name: 'たくみん',
       email: 'example.acom',
+      description: '私の名前は、たくみんです',
     },
   });
 
   const takumi2 = await prisma.user.create({
     data: {
-      id: 'user-id-4',
-      name: 'takuaka',
+      id: 'user-id-3',
+      name: 'みくた',
       email: 'examdple.com',
+      description: '最近あついのよ',
     },
   });
 
   const akasaka = await prisma.user.create({
     data: {
-      id: 'user-id-2',
-      name: 'sakasu',
+      id: 'user-id-4',
+      name: 'さかすすすす',
       email: 'example1.com',
+      description: 'ブラック部活について語っています',
     },
   });
 }
--- frontend/src/component/user/user.tsx
+++ frontend/src/component/user/user.tsx
@@ -1,4 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Users(): any {
   const EXCHANGE_RATES = gql`
@@ -18,13 +20,24 @@ function Users(): any {
   if (error) return `Error : ${error}`;
 
   return data.users.map(({ id, name, email, createdAt, updatedAt }: any) => (
-    <div key={id}>
-      <p>{id}</p>
-      <p>{name}</p>
-      <p>{email}</p>
-      <p>{createdAt}</p>
-      <p>{updatedAt}</p>
-    </div>
+    <Card style={{ width: "18rem" }} key={id}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">詳しく見る</Button>
+      </Card.Body>
+    </Card>
+    // <div key={id}>
+    //   <p>{id}</p>
+    //   <p>{name}</p>
+    //   <p>{email}</p>
+    //   <p>{createdAt}</p>
+    //   <p>{updatedAt}</p>
+    // </div>
   ));
 }
 export default Users;
