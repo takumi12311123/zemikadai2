d03c6992f52afc9e7856d7c365ef4347e2a269fd
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-07-19 09:28:41 +0900

    userArticlesの作成

--- frontend/src/component/header/header.tsx
+++ frontend/src/component/header/header.tsx
@@ -1,23 +1,27 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
 export const Header = () => {
   const user = "takumi akasaka";
+
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand href="/">fullstack app</Navbar.Brand>
-        <Navbar.Toggle />
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="#features">Following</Nav.Link>
-        <Nav.Link href="#pricing">MyPage</Nav.Link>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">{user}</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <BrowserRouter>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="/">fullstack app</Navbar.Brand>
+          <Navbar.Toggle />
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href={`/following`}>Following</Nav.Link>
+          <Nav.Link href={`/user`}>MyPage</Nav.Link>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">{user}</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </BrowserRouter>
   );
 };
