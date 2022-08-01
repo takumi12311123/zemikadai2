6274cdcf102f11ac8d279569daf4d4de920baf76
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-08-01 20:40:30 +0900

    buttonの微修正

--- frontend/src/component/button/UserDetailButton.tsx
+++ frontend/src/component/button/UserDetailButton.tsx
@@ -1,9 +1,9 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 
 export const UserDetailButton = () => {
 
   const getUserDetail = () => {
-    
   };
 
   return <div onClick={getUserDetail}>UserDetailButton</div>;
