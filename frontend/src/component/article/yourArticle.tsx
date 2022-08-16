c4ed138f02dffd2cf60682f1169d2a02a0866ccc
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-08-16 21:42:16 +0900

    いらないもの削除

--- frontend/src/component/article/yourArticle.tsx
+++ frontend/src/component/article/yourArticle.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
 export const yourArticle = () => {
-  return (
-    <div>yourArticle</div>
-  )
-}
+  return <div>yourArticle</div>;
+};
--- frontend/src/component/button/FavoriteButton.tsx
+++ frontend/src/component/button/FavoriteButton.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const FavoriteButton = () => {
   return <div>FavoriteButton</div>;
 };
