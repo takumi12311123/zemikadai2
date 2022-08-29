ff3582678c850a1c2a9677dd63030b94fa007da9
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-08-29 21:47:35 +0900

    butonの部分微修正

--- frontend/src/component/button/UserDetailButton.tsx
+++ frontend/src/component/button/UserDetailButton.tsx
@@ -1,14 +1,7 @@
-import { gql } from "@apollo/client";
-
 export const UserDetailButton = () => {
   function getUserDetail(userId: string): any {
-    gql``;
-    // userDetailを取得するgqlをここに置く
-    // どうやって、userIdを取得するか考える
-    // userIdをkeyに置きたい
-    // この問題を送った側で処理するようになるかも
     return;
   }
 
-  return <div onClick={getUserDetail("user-id-1")}>UserDetailButton</div>;
+  return <div onClick={getUserDetail("")}>UserDetailButton</div>;
 };
